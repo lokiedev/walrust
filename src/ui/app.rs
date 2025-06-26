3f62e7496d6f90e508030972410891288142bff9
lokiedev <lokie.acc@proton.me>
2025-06-26 16:11:56 +0800

    refactor: better code readability

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -11,15 +11,15 @@ use ratatui::{
 };
 use std::{error::Error, path::PathBuf, time::Duration};
 
-const POLL_TIMEOUT_MS: u64 = 0;
-const THREAD_SLEEP_DURATION_MS: u64 = 16; // ~60fps
+const TARGET_FPS: u64 = 60;
+const FRAME_DURATION_MS: u64 = 1000 / TARGET_FPS; // ~60fps
 
 const PREVIEW_WIDTH_PERCENT: u16 = 40;
 const SELECTOR_WIDTH_PERCENT: u16 = 60;
 const DEFAULT_WALLPAPER_PATH: &str = "pictures/wallpapers";
 
-static VERTICAL_CONSTRAINT: [Constraint; 1] = [Constraint::Fill(1)];
-static HORIZONTAL_CONTRAINT: [Constraint; 2] = [
+static VERTICAL_LAYOUT: [Constraint; 1] = [Constraint::Fill(1)];
+static HORIZONTAL_LAYOUT: [Constraint; 2] = [
     Constraint::Percentage(PREVIEW_WIDTH_PERCENT),
     Constraint::Percentage(SELECTOR_WIDTH_PERCENT),
 ];
@@ -34,12 +34,12 @@ pub struct App {
 
 impl App {
     pub fn new() -> AppResult<Self> {
-        // Initializing Selector component
+        // Create and initialize selector component
         let wallpapers = Self::load_wallpaper(DEFAULT_WALLPAPER_PATH)?;
         let mut selector = Selector::new(wallpapers);
         selector.init();
 
-        // Initializing Preview component
+        // Create preview component
         let preview = Preview::new()?;
 
         log::info!("App object created");
@@ -51,55 +51,54 @@ impl App {
     }
 
     pub fn run(&mut self, mut terminal: DefaultTerminal) -> AppResult<()> {
-        // Main loop
+        // Application main loop
         while !self.should_quit {
             self.handle_events()?;
 
             let _ = terminal.draw(|frame| self.draw(frame));
 
-            std::thread::sleep(Duration::from_millis(THREAD_SLEEP_DURATION_MS));
+            // Limit loop speed
+            std::thread::sleep(Duration::from_millis(FRAME_DURATION_MS));
         }
 
         Ok(())
     }
 
+    // Main draw method (each UI component must has this method)
     fn draw(&mut self, frame: &mut Frame) {
-        let inner_content_area = Self::draw_main_border(frame);
+        let content_area = Self::draw_border(frame);
 
         let [preview_area, selector_area] =
-            Layout::horizontal(HORIZONTAL_CONTRAINT).areas(inner_content_area);
+            Layout::horizontal(HORIZONTAL_LAYOUT).areas(content_area);
 
-        // Draw selector components
+        // Draw components
         self.selector.draw(frame, selector_area);
-
-        // Draw preview components
         let selected_wallpaper = self.selector.get_selected_wallpaper();
         let _ = self.preview.draw(selected_wallpaper, frame, preview_area);
     }
 
-    fn draw_main_border(frame: &mut Frame) -> ratatui::layout::Rect {
-        let [main_layout_area] = Layout::vertical(VERTICAL_CONSTRAINT)
+    fn draw_border(frame: &mut Frame) -> ratatui::layout::Rect {
+        let [layout_area] = Layout::vertical(VERTICAL_LAYOUT)
             .margin(1)
             .areas(frame.area());
-        let border_block = Block::bordered()
+
+        let border = Block::bordered()
             .border_type(ratatui::widgets::BorderType::Rounded)
             .title(Line::from("Select Wallpaper").centered());
-        let inner_content_area = border_block.inner(main_layout_area);
+        let content_area = border.inner(layout_area);
 
-        frame.render_widget(border_block, main_layout_area);
+        frame.render_widget(border, layout_area);
 
-        inner_content_area
+        content_area
     }
 
     fn handle_events(&mut self) -> AppResult<()> {
-        log::debug!("Checking event poll");
-        if !crossterm::event::poll(Duration::from_millis(POLL_TIMEOUT_MS))? {
-            log::debug!("Event not available");
+        if !crossterm::event::poll(Duration::ZERO)? {
             return Ok(());
         }
 
         if let Event::Key(key) = event::read()? {
-            log::debug!("Key {} clicked by user", &key.code);
+            log::debug!("Key '{}' pressed", &key.code);
 
             if let Some(action) = self.handle_key(key) {
                 self.handle_action(action)?;
@@ -112,18 +111,11 @@ impl App {
     fn handle_action(&mut self, action: Action) -> AppResult<()> {
         match action {
             Action::Quit => self.should_quit = true,
-            Action::NextItem => {
-                log::debug!("Next item action");
-                self.selector.list_state.select_next();
-            }
-            Action::PreviousItem => {
-                log::debug!("Previous item action");
-                self.selector.list_state.select_previous();
-            }
-            Action::SelectItem(wallpaper_path) => {
-                log::debug!("Select item action");
-                if let Err(e) = change_wallpaper(&wallpaper_path) {
-                    log::error!("{}", e);
+            Action::NextItem => self.selector.list_state.select_next(),
+            Action::PreviousItem => self.selector.list_state.select_previous(),
+            Action::SelectItem(path) => {
+                if let Err(e) = change_wallpaper(&path) {
+                    log::error!("Failed to change wallpaper: {}", e);
                 }
             }
         }
@@ -133,26 +125,15 @@ impl App {
 
     #[inline]
     fn handle_key(&mut self, key: KeyEvent) -> Option<Action> {
-        // Handle global key
         match key.code {
-            KeyCode::Char('q') => return Some(Action::Quit),
-            // Handle other global key here
-            _ => {}
+            KeyCode::Char('q') => Some(Action::Quit),
+            _ => self.selector.handle_key(key),
         }
-
-        // Handle specific area key
-        if let Some(action) = self.selector.handle_key(key) {
-            return Some(action);
-        }
-
-        None
     }
 
     fn load_wallpaper(dir_path: &str) -> AppResult<Vec<Wallpaper>> {
         let wallpaper_dir: PathBuf = get_home_dir()?.join(dir_path);
-        let wallpaper_dir_str = wallpaper_dir
-            .to_str()
-            .ok_or("Failed to convert home directory path to string")?;
-        Loader::load_wallpaper(wallpaper_dir_str)
+        let dir_str = wallpaper_dir.to_str().ok_or("Invalid wallpaper path")?;
+        Loader::load_wallpaper(dir_str)
     }
 }
