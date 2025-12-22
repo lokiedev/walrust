1bd3c9879af1ecae5edd5acc9803fac723520125
lokiedev <lokie.acc@proton.me>
2025-12-22 15:50:39 +0800

    Initial Commit

---
+++ src/models/image_file.rs
@@ -0,0 +1,19 @@
+use std::path::PathBuf;
+
+struct ImageFile {
+    name: PathBuf,
+    folder: PathBuf,
+}
+
+impl ImageFile {
+    pub fn new(name: impl Into<PathBuf>, folder: impl Into<PathBuf>) -> Self {
+        ImageFile {
+            name: name.into(),
+            folder: folder.into(),
+        }
+    }
+
+    pub fn path(&self) -> PathBuf {
+        self.folder.join(&self.name)
+    }
+}
