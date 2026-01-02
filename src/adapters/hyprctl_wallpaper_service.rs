4904f382b7a7f139d05145b89e721dff22d832bf
lokiedev <lokie.acc@proton.me>
2026-01-02 11:47:58 +0800

    fix(hyprctl): handle both stdout and stderr for error message

--- src/adapters/hyprctl_wallpaper_service.rs
+++ src/adapters/hyprctl_wallpaper_service.rs
@@ -22,7 +22,12 @@ impl HyprctlWallpaperService {
         let output = Self::hyprctl(&[arg, flag])?;
 
         if !output.status.success() {
-            let error_message = String::from_utf8(output.stderr).unwrap();
+            let error_message = if output.stderr.is_empty() {
+                String::from_utf8_lossy(&output.stdout)
+            } else {
+                String::from_utf8_lossy(&output.stderr)
+            };
+
             return Err(anyhow!("hyprctl command returned error: {}", error_message));
         }
 
@@ -43,6 +48,14 @@ impl HyprctlWallpaperService {
         }
     }
 
+    // Hyprctl sends its error output to stdout instead of stderr.
+    // So to get the error message, you can use the stdout only,
+    // or you can handle both for example:
+    // if output.stderr.is_empty() {
+    //    do something with the stdout
+    // } else {
+    //    do something with the stderr
+    // }
     fn hyprctl(args: &[&str]) -> Result<Output> {
         Command::new("hyprctl")
             .args(args)
@@ -73,7 +86,11 @@ impl WallpaperServicePort for HyprctlWallpaperService {
         ensure!(
             change_wallpaper_output.status.success(),
             "hyprctl command returned error: {}",
-            String::from_utf8_lossy(&change_wallpaper_output.stderr)
+            if change_wallpaper_output.stderr.is_empty() {
+                String::from_utf8_lossy(&change_wallpaper_output.stdout)
+            } else {
+                String::from_utf8_lossy(&change_wallpaper_output.stderr)
+            }
         );
 
         Ok(())
