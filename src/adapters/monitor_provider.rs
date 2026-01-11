ff68c09c5cc08856649acf83da6955c7d1ba3f6d
lokiedev <lokie.acc@proton.me>
2026-01-11 11:34:26 +0800

    refactor(adapters): add MonitorProvider from Desktop method

--- src/adapters/monitor_provider.rs
+++ src/adapters/monitor_provider.rs
@@ -1,7 +1,18 @@
-use crate::{adapters::hyprctl::HyprctlMonitorProvider, ports::MonitorProviderPort};
+use crate::{
+    adapters::hyprctl::HyprctlMonitorProvider, models::desktop::Desktop, ports::MonitorProviderPort,
+};
 
 pub enum MonitorProvider {
     Hyprctl(HyprctlMonitorProvider),
+}
+
+impl MonitorProvider {
+    fn from(desktop: Desktop) -> Option<Self> {
+        match desktop {
+            Desktop::Hyprland => Some(MonitorProvider::Hyprctl(HyprctlMonitorProvider)),
+            _ => None,
+        }
+    }
 }
 
 impl MonitorProviderPort for MonitorProvider {
