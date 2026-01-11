ee11ddf04042be4c3710d7f053fba66d3a11f208
Lokie <lokie.acc@proton.me>
2026-01-11 12:14:18 +0800

    feat: desktop detection (#10)
    * refactor(models): add desktop model
    
    * refactor(adapters): add MonitorProvider from Desktop method
    
    * refactor(adapters): add WallpaperService from Desktop method
    
    * refactor: use Desktop::detect

---
+++ src/models/desktop.rs
@@ -0,0 +1,45 @@
+use std::env;
+
+pub enum Desktop {
+    Hyprland,
+    Unknown,
+}
+
+impl Desktop {
+    pub fn from(wm_name: &str) -> Self {
+        match wm_name {
+            "Hyprland" => Desktop::Hyprland,
+            _ => Desktop::Unknown,
+        }
+    }
+
+    pub fn from_env(env_var: &str) -> Self {
+        if let Ok(wm_name) = env::var(env_var) {
+            return Desktop::from(&wm_name);
+        }
+        Desktop::Unknown
+    }
+
+    pub fn detect() -> Self {
+        let desktop = Desktop::from_env("CURRENT_DESKTOP");
+        if !desktop.is_unknown() {
+            return desktop;
+        }
+
+        let desktop = Desktop::from_env("XDG_SESSION_DESKTOP");
+        if !desktop.is_unknown() {
+            return desktop;
+        }
+
+        let desktop = Desktop::from_env("XDG_CURRENT_DESKTOP");
+        if !desktop.is_unknown() {
+            return desktop;
+        }
+
+        Desktop::Unknown
+    }
+
+    pub fn is_unknown(&self) -> bool {
+        matches!(*self, Desktop::Unknown)
+    }
+}
